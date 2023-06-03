31f92fe66ebed0017bac783e72c6f9f51de46e9d
Woopertail <woopertail@gmail.com>
2023-06-03 11:07:47 +0300

    1.0.8 -> 1.0.9

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 
 setup(name='FunPayAPI',
-      version="1.0.7",
+      version="1.0.9",
       description='Прослойка между FunPayAPI и клиентом.',
       long_description=long_desc,
       long_description_content_type="text/markdown",
@@ -14,7 +14,7 @@ setup(name='FunPayAPI',
       author_email='woopertail@gmail.com',
       url='https://github.com/woopertail/FunPayAPI',
       packages=find_packages("."),
-      license='GPL2',
+      license='GPL3',
       keywords='funpay bot api tools',
       install_requires=['requests==2.28.1', 'beautifulsoup4', 'requests_toolbelt==0.10.1'],
       classifiers=[
